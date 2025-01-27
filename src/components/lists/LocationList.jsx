21894a80671fe3ea475ec1b847470c62318b8ff8
Nate Z <ntzeitlin@gmail.com>
2025-01-27 11:58:55 -0600

    current user now tracked

---
+++ src/components/lists/LocationList.jsx
@@ -0,0 +1,8 @@
+// check to see who the current user is
+// find locations related to that current user
+// list them out as cards
+
+
+export const LocationList = () => {
+   return () 
+}
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -2,8 +2,17 @@ import { Outlet, Route, Routes } from "react-router-dom";
 import { TestComponent } from "../components/detail/TestComponent";
 import { NavBar } from "../components/nav/NavBar";
 import { Footer } from "../components/nav/Footer";
+import { useEffect, useState } from "react";
 
 export const ApplicationViews = () => {
+    const [currentUser, setCurrentUser] = useState({});
+
+    useEffect(() => {
+        const localUser = localStorage.getItem("attention_astronaut");
+        const localUserObject = JSON.parse(localUser);
+        setCurrentUser(localUserObject);
+    }, []);
+
     return (
         <Routes>
             <Route
@@ -22,19 +31,35 @@ export const ApplicationViews = () => {
                 />
                 <Route
                     path="locations"
-                    element={<TestComponent currentLocation={"Locations"} />}
+                    element={
+                        <TestComponent
+                            currentLocation={`Locations for user #${currentUser.id}`}
+                        />
+                    }
                 ></Route>
                 <Route
                     path="projects"
-                    element={<TestComponent currentLocation={"Projects"} />}
+                    element={
+                        <TestComponent
+                            currentLocation={`Projects for user #${currentUser.id}`}
+                        />
+                    }
                 ></Route>
                 <Route
                     path="inventory"
-                    element={<TestComponent currentLocation={"Inventory"} />}
+                    element={
+                        <TestComponent
+                            currentLocation={`Inventory for user #${currentUser.id}`}
+                        />
+                    }
                 ></Route>
                 <Route
                     path="profile"
-                    element={<TestComponent currentLocation={"Profile"} />}
+                    element={
+                        <TestComponent
+                            currentLocation={`Profile for user #${currentUser.id}`}
+                        />
+                    }
                 ></Route>
             </Route>
         </Routes>
