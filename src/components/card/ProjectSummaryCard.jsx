3591279437f1edb09818570b6f6b71c2f86c0421
Nate Z <ntzeitlin@gmail.com>
2025-01-28 14:44:04 -0600

    users can now click a project name from the location details view to be taken to the project details view

--- src/components/card/ProjectSummaryCard.jsx
+++ src/components/card/ProjectSummaryCard.jsx
@@ -1,5 +1,17 @@
 import { Card } from "@radix-ui/themes";
+import { Link, useParams } from "react-router-dom";
 
 export const ProjectSummaryCard = ({ projectObject }) => {
-    return <Card m="2">{projectObject.project?.name}</Card>;
+    const { locationId } = useParams();
+
+    return (
+        <Card m="2">
+            <Link
+                to={`/project/${projectObject.project?.id}`}
+                state={locationId}
+            >
+                {projectObject.project?.name}
+            </Link>
+        </Card>
+    );
 };
