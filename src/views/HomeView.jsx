1a5f3190ab9770d1bb3a08d2ebe5f055c8bae9fb
Nate Z <ntzeitlin@gmail.com>
2025-01-30 16:03:51 -0600

    added a homepage

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,3 +1,16 @@
+import galaxyImage from "../assets/AttentionGalaxy.jpg";
 export const HomeView = () => {
-    return <>This is a home view</>;
+    const backgroundStyle = {
+        backgroundImage: `url(${galaxyImage})`,
+        backgroundPosition: "center",
+        height: "80vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "yellow",
+        textAlign: "center",
+        fontSize: "18em",
+    };
+
+    return <div style={backgroundStyle}>Attention Galaxy</div>;
 };
