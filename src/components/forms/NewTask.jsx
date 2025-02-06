74dfae1dc4519b5fdf48e0e6b00d558721ba4edf
Nate Z <ntzeitlin@gmail.com>
2025-02-06 09:28:30 -0600

    removing zombie code from NewTask form

--- src/components/forms/NewTask.jsx
+++ src/components/forms/NewTask.jsx
@@ -2,30 +2,14 @@ import { useParams } from "react-router-dom";
 
 import { Container, Flex } from "@radix-ui/themes";
 import { EditTaskCard } from "./EditTaskCard";
-import { TaskItemListCard } from "../card/tasks/TaskItemList";
-import { useEffect, useState } from "react";
-import { getProjectAndLocationDataByTaskId } from "../../services/taskService";
 
-export const NewTask = ({ currentUser }) => {
+export const NewTask = () => {
     const { taskId } = useParams();
-
-    const [projectLocationData, setProjectLocationData] = useState({});
-
-    useEffect(() => {
-        getProjectAndLocationDataByTaskId(taskId).then((data) =>
-            setProjectLocationData(data)
-        );
-    }, [taskId]);
 
     return (
         <Container width="60%" m="5">
             <Flex gap="4" justify="center">
                 <EditTaskCard taskId={taskId} />
-                {/* <TaskItemListCard
-                    taskId={taskId}
-                    locationId={projectLocationData.locationId}
-                    currentUser={currentUser}
-                /> */}
             </Flex>
         </Container>
     );
