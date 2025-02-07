f5a725f038220be32f084c9d4f4ec9d5e1c3e35d
Nate Z <ntzeitlin@gmail.com>
2025-02-07 11:32:44 -0600

    added loading..

--- src/components/detail/ProjectDetailView.jsx
+++ src/components/detail/ProjectDetailView.jsx
@@ -1,5 +1,12 @@
 /* eslint-disable react/prop-types */
-import { Button, Card, Container, Flex, Heading } from "@radix-ui/themes";
+import {
+    Container,
+    Flex,
+    Section,
+    Box,
+    Heading,
+    Spinner,
+} from "@radix-ui/themes";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProjectInfoByProjectId } from "../../services/projectService";
@@ -10,6 +17,9 @@ import { MiseEnPlaceListCard } from "../card/tasks/MiseEnPlaceList";
 
 export const ProjectDetail = ({ currentUser }) => {
     const { projectId } = useParams();
+
+    const [loading, setLoading] = useState(true);
+
     const [projectData, setProjectData] = useState({
         name: "",
         startdate: "",
@@ -29,9 +39,10 @@ export const ProjectDetail = ({ currentUser }) => {
     }, [currentUser]);
 
     useEffect(() => {
-        getLocationInfoByProjectId(projectId).then((data) =>
-            setLocationData(data[0])
-        );
+        getLocationInfoByProjectId(projectId).then((data) => {
+            setLocationData(data[0]);
+            setLoading(false);
+        });
     }, [projectId]);
 
     const fetchAndSetProjectData = () => {
@@ -42,23 +53,34 @@ export const ProjectDetail = ({ currentUser }) => {
 
     return (
         <Container width="90%" m="5">
-            <Flex gap="4" justify="center">
-                <ProjectDetailCard
-                    currentUser={currentUser}
-                    locationData={locationData}
-                    projectData={projectData}
-                    fetchAndSetProjectData={fetchAndSetProjectData}
-                />
-                <TaskListCard
-                    projectId={projectId}
-                    locationData={locationData}
-                    currentUser={currentUser}
-                />
-                <MiseEnPlaceListCard
-                    currentUser={currentUser}
-                    projectId={projectId}
-                />
-            </Flex>
+            {loading ? (
+                <Section mt="9">
+                    <Flex align="center" justify="center">
+                        <Box mr="3">
+                            <Spinner size="3" />
+                        </Box>
+                        <Heading size="8">Loading...</Heading>
+                    </Flex>
+                </Section>
+            ) : (
+                <Flex gap="4" justify="center">
+                    <ProjectDetailCard
+                        currentUser={currentUser}
+                        locationData={locationData}
+                        projectData={projectData}
+                        fetchAndSetProjectData={fetchAndSetProjectData}
+                    />
+                    <TaskListCard
+                        projectId={projectId}
+                        locationData={locationData}
+                        currentUser={currentUser}
+                    />
+                    <MiseEnPlaceListCard
+                        currentUser={currentUser}
+                        projectId={projectId}
+                    />
+                </Flex>
+            )}
         </Container>
     );
 };
