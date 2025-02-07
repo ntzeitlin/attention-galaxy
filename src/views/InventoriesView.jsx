113d6bc853aceac6596df09e9976facc37e860cd
Nate Z <ntzeitlin@gmail.com>
2025-02-07 10:41:49 -0600

    added loading to inventoriesView

--- src/views/InventoriesView.jsx
+++ src/views/InventoriesView.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import { getLocationsByUserId } from "../services/locationService";
-import { Container, Heading, Section } from "@radix-ui/themes";
+import {
+    Box,
+    Container,
+    Flex,
+    Heading,
+    Section,
+    Spinner,
+} from "@radix-ui/themes";
 import { LocationInventoryList } from "../components/inventoriesview/LocationInventoryList";
 
 export const InventoryListView = ({ currentUser }) => {
     const [locationArray, setLocationArray] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        getLocationsByUserId(currentUser.id).then((data) =>
-            setLocationArray(data)
-        );
+        getLocationsByUserId(currentUser.id).then((data) => {
+            setLocationArray(data);
+            setLoading(false);
+        });
     }, [currentUser]);
 
     return (
@@ -18,10 +27,21 @@ export const InventoryListView = ({ currentUser }) => {
                 Location Inventories
             </Heading>
             <Section>
-                <LocationInventoryList
-                    currentUser={currentUser}
-                    locationArray={locationArray}
-                />
+                {loading ? (
+                    <Section mt="9">
+                        <Flex align="center" justify="center">
+                            <Box mr="3">
+                                <Spinner size="3" />
+                            </Box>
+                            <Heading size="8">Loading...</Heading>
+                        </Flex>
+                    </Section>
+                ) : (
+                    <LocationInventoryList
+                        currentUser={currentUser}
+                        locationArray={locationArray}
+                    />
+                )}
             </Section>
         </Container>
     );
